eacb8e851bbbec7c85af06caac4fbd55e9fb029d
h-ibo <hibrahimgv@gmail.com>
2026-03-01 21:45:43 +0300

    backend: docker, env ve olaylar endpoint eklendi

--- app/database.py
+++ app/database.py
@@ -1,24 +1,22 @@
+import os
+from dotenv import load_dotenv
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-# Veritabanı URL
-DATABASE_URL = "postgresql+asyncpg://safekampus_user:Kartal1903!@localhost/safekampus_db"
+load_dotenv()
 
-# Async engine
+DATABASE_URL = os.getenv("DATABASE_URL")
+
 engine = create_async_engine(DATABASE_URL, echo=True)
 
-# Session oluşturucu
 SessionLocal = sessionmaker(
     bind=engine,
     class_=AsyncSession,
     expire_on_commit=False
 )
 
-# Base class
 Base = declarative_base()
 
-
-# FastAPI'nin her endpoint için DB oturumu oluşturup kapatmasını sağlayan fonksiyon
 async def get_db():
     async with SessionLocal() as session:
         try:
--- app/utils/token.py
+++ app/utils/token.py
@@ -1,14 +1,17 @@
+import os
+from dotenv import load_dotenv
 from datetime import datetime, timedelta
-from jose import jwt 
+from jose import jwt
 
-SECRET_KEY = "safekampus_super_secret_key_123"  # ileride .env'ye alınacak
-ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = 60
+load_dotenv()
+
+SECRET_KEY = os.getenv("SECRET_KEY")
+ALGORITHM = os.getenv("ALGORITHM")
+ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES"))
 
 def create_access_token(data: dict):
     to_encode = data.copy()
     expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
-    to_encode["exp"] = int(expire.timestamp()) 
-
+    to_encode["exp"] = int(expire.timestamp())
     encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
     return encoded_jwt
